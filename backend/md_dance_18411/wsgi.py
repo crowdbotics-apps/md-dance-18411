8af367c8b1d1cda404d267cd0d0ec61a3434535b
Crowdbotics <team@crowdbotics.com>
2020-06-25 06:18:28 +0000

    Update models

--- backend/md_dance_18411/wsgi.py
+++ backend/md_dance_18411/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'md_dance_18411.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "md_dance_18411.settings")
 
 application = get_wsgi_application()
